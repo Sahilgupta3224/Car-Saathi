61e9db19121c6c5cc05fe71e2678cae88fff172e
kr1shr1 <srikritarth@gmail.com>
2024-03-12 19:20:52 +0530

    Signup Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
-import logo from './logo.svg';
 import Signup from './pages/Signup/Signup.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 
 function App() {
   return (
@@ -10,6 +15,8 @@ function App() {
        <BrowserRouter>
         <Routes>
           <Route path="/" element={<Signup/>} />
+          <Route path="/Signup" element={<Signup/>} />
+          
         </Routes>
       </BrowserRouter>   
     </>
