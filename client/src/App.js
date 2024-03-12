7942280e9fa1476d105780b04b760eaabe64c7f5
Rudra1sharma <sharmarudra761@gmail.com>
2024-03-12 23:20:31 +0530

    created signin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import Signup from './pages/Signup/Signup.jsx';
+import SignIn from './pages/SignIn/SignIn.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
 
 
 
 function App() {
   return (
-  
-      <>
+    <>
        <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Signup/>} />
+
+          <Route path="/" element={<SignIn />} />
+          <Route path="/SignIn" element={<SignIn/>} />
+          
           <Route path="/Signup" element={<Signup/>} />
           
         </Routes>
