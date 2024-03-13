a6c790d55e313c375a12f4b32575ed3ae3d3d4e6
Rudra1sharma <sharmarudra761@gmail.com>
2024-03-13 15:22:14 +0530

    added some functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Signup from './pages/Signup/Signup.jsx';
 import SignIn from './pages/SignIn/SignIn.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './pages/Dashboard/dashboard.jsx';
 // import '@fontsource/roboto/300.css';
 // import '@fontsource/roboto/400.css';
 // import '@fontsource/roboto/500.css';
@@ -17,8 +18,10 @@ function App() {
 
           <Route path="/" element={<SignIn />} />
           <Route path="/SignIn" element={<SignIn/>} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
           
-          <Route path="/Signup" element={<Signup/>} />
+          <Navbar/>
           
         </Routes>
       </BrowserRouter>   
---
+++ client/src/components/Navitem.jsx
@@ -0,0 +1,47 @@
+export const navItems = [
+    {
+        id: 1,
+        title: "Home",
+        path: "./",
+        cName: "Nav-item",
+    },
+    {
+        id: 2,
+        title: "Services",
+        path: "./Services",
+        cName: "Nav-item",
+    },
+    {
+        id: 3,
+        title: "Contact Us",
+        path: "./Contact Us",
+        cName: "Nav-item",
+    }
+];
+
+export const ServiceDropdown = [
+    {
+        id: 1,
+        title: "Your Rides",
+        path: "./Your Rides",
+        cName: "submenu-item",
+    },
+    {
+        id: 2,
+        title: "Inbox",
+        path: "./Inbox",
+        cName: "submenu-item",
+    },
+    {
+        id: 3,
+        title: "Profile",
+        path: "./Profile",
+        cName: "submenu-item",
+    },
+    {
+        id: 4,
+        title: "Log Out",
+        path: "./Log Out",
+        cName: "submenu-item",
+    },
+];
